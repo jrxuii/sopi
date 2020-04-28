3b6235a63b41abe252d9cd3348791926018034b4
jrxui <34170472+jrxui@users.noreply.github.com>
2020-04-28 12:05:28 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9; SM-M105G Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.111 Mobile Safari/537.36 Shopee Beeshop locale/id version=340 appver=25416 rnver=1586793521";
+$DeviceId = "Ptk3jEHTCW107U2w6jE26nbA75m86lQNSlglEU1v2cU%3D";
+$SPCF = "SPC_F=960813189984257_15e8a253bda2bdc3_79d6e45071285055";
+$SPCAFTID = "SPC_AFTID=a06685cd-f32c-4e18-be0b-2ca82872dca1";
+$UserId = "158858429";
+$Username = "zilansh";
+$CsrfToken = "csrftoken=BjAlMZNRJ57ZIiyAEDLypTCp63fIRCp9";
+$ShopeeToken = "shopee_token=kAEJxKQ2PnhzfFag+ymbJr4PB9oxWKyvPlhE0ydQrtiOEVwct12hW3ifBwDjECG0";
+$SPCEC = 'SPC_EC="/Dqcpw/gLiHRoqjxS0FIiTTTz53jm5z8OHnccwLkR+c94OeycMoI4KwsRYx+VHyR9VD1Q4hq5IJOC/7lIlD9CYynDybFiup+F7ufLId0nubYUSn/jBdw9SCYaFZirq5t8e2UaU5scX433Ai733wJlJ0DrWrCCEcRKFX2QCN0/2w="';
